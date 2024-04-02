f1a6bcb542d9784088b113a854deaaa34d54afe0
Srushti2411 <srushtigarad24@gmail.com>
2024-04-02 20:47:45 +0530

    Added some basic code in Tabletpage

--- client/src/views/Tablet/Tablet.js
+++ client/src/views/Tablet/Tablet.js
@@ -1,13 +1,37 @@
-import './Tablet.css'
-import Navbar from '../../components/Navbar/Navbar'
+import React from 'react';
+import './Tablet.css'; 
+import TabletCard from '../../components/TabletCard/TabletCard';
+import Navbar from '../../components/Navbar/Navbar';
+
+const tabletsData = [
+  {
+    id: 1,
+    name: 'iPad Pro',
+    brand: 'Apple',
+    price: 799,
+    imageUrl: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 2,
+    name: 'Galaxy Tab S7+',
+    brand: 'Samsung',
+    price: 849,
+    imageUrl: 'https://via.placeholder.com/150',
+  },
+];
 
 function Tablet() {
   return (
     <div>
-        <Navbar />
-        <h1 style={{textAlign: 'center',margin: '150px',fontSize: '70px'}}> Tablet 🏡 </h1>
+      <Navbar /> {}
+      <h1>Tablets</h1>
+      <div className="tablets-container">
+        {tabletsData.map((tablet) => (
+          <TabletCard key={tablet.id} tablet={tablet} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Tablet
+export default Tablet;
