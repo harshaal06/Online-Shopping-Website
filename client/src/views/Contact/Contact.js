d954a0e8b589cc26a3a9e2cb038ccb8a05c25e3b
Wagh Ganesh <waghganesh580@gmail.com>
2024-04-07 17:42:57 +0530

    image added

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./Contact.css"
-import email from "../../contact/email.png"
-import phone from "../../contact/phone.png"
-import location from "../../contact/location.png"
-import shape from "../../contact/shape.png"
+import email from "../../views/Contact/email.png"
+import phone from "../../views/Contact/phone.png"
+import location from "../../views/Contact/location.png"
+import shape from "../../views/Contact/shape.png"
 export default function Contact() {
     
     
